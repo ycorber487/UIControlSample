7cc091ebc858a925e40d687a5ed38f154ad7352f
ycorber487 <163828691+ycorber487@users.noreply.github.com>
2024-12-08 12:51:53 +0100

    Add files via upload

---
+++ UIControl/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module es.ieslosmontecillos.uicontrolsample {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.logging;
+    requires java.desktop;
+    requires javafx.graphics;
+
+    opens es.ieslosmontecillos.uicontrolsample to javafx.fxml;
+    exports es.ieslosmontecillos.uicontrolsample.tooltipsample;
+    exports es.ieslosmontecillos.uicontrolsample.checkboxsample;
+    exports es.ieslosmontecillos.uicontrolsample.tableviewsample;
+    exports es.ieslosmontecillos.uicontrolsample.listviewsample;
+
+}
